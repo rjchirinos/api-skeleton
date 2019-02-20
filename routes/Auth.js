fdd09a34896badc2ee6f248f97958948b82e662b
Raul <raulch1998@gmail.com>
2019-02-20 12:06:57 -0400

    Add user authentication logic

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
@@ -36,5 +37,32 @@ module.exports = app => {
       };
     });
     res.send(result);
+  });
+
+  app.post('/api/auth', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).send('This user does not exist...');
+      throw new Error('This user does not exist...');
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      res.status(401).res('Incorrect password...');
+      throw new Error('Incorrect password...');
+    }
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        email: user.email
+      },
+      process.env.TOKEN,
+      { expiresIn: '1h' }
+    );
+    res.send({
+      userId: user._id,
+      token,
+      tokenExpiration: 1
+    });
   });
 };
