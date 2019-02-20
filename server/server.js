70fd50bce57af9931715e4643c2b02d21901bde5
Raul <raulch1998@gmail.com>
2019-02-20 10:02:38 -0400

    Add user model

--- server/server.js
+++ server/server.js
@@ -1,7 +1,25 @@
 const app = require('./express');
+const mongoose = require('./mongoose');
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  var isDevMode = true;
+}
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, err => {
-  if (err) console.log(err);
-  console.log(`App running on port ${PORT}...`);
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-4zajv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App running in port ${PORT}...`);
+      if (isDevMode) console.log('App is in development mode...');
+    });
+  })
+  .catch(err => {
+    console.error('Unable to connect to MongoDB', err);
+  });
